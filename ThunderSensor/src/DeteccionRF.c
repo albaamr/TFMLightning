82906fdfe2879dd3ea7bdbd533359498eec2d153
albaamr <albmor13@ucm.es>
2025-03-18 17:02:43 +0100

    Prueba de sensores AS3935. Detección completa

--- ThunderSensor/src/DeteccionRF.c
+++ ThunderSensor/src/DeteccionRF.c
@@ -7,6 +7,13 @@
 #include <gpiod.h>  // Necesario para la interrupción. Librería para manejo de gpios libgpiod
 //Con comillas busca en el propio proyecto
 
+// FUNCIONES, REGISTROS, CONFIG, INTERACCION SENSOR FICHERO APARTE .C aqui no deberia haber referencia a raspberry pi
+// Meter gpiochip, gpioirq, en un struct
+// Hacerlo en otra carpeta, mantener este código. Empezar con el main
+// 3 ficheros: raspi, app y sensor
+// hablar con fernando representación de datos. practica
+// no es estrictamente necesario hacer pcb (15 mayo aprox para que llegue). PROTOTIPO DE SISTEMA DE DETECCIÓN DE RAYOS
+
 //-----------DEFINES-----------
 // GPIO 17 en la Raspberry Pi Zero 2 W
 #define GPIO_CHIP "/dev/gpiochip0"
@@ -23,6 +30,45 @@
 #define NORMAL_MODE 0x00
 #define POWERDOWN_MODE 0x01
 
+//Watchdog threshold. Cuando más grande, más robusto contra el ruido
+#define CONFIG_WDTH_0 0x00
+#define CONFIG_WDTH_1 0x01
+#define CONFIG_WDTH_2 0x02 //Default
+#define CONFIG_WDTH_3 0x03
+#define CONFIG_WDTH_4 0x04
+#define CONFIG_WDTH_5 0x05
+#define CONFIG_WDTH_6 0x06
+#define CONFIG_WDTH_7 0x07
+#define CONFIG_WDTH_8 0x08
+#define CONFIG_WDTH_9 0x09
+#define CONFIG_WDTH_10 0x0A
+
+//Noise Floor Level threshold
+#define CONFIG_NFLT_0 0x00
+#define CONFIG_NFLT_1 0x10
+#define CONFIG_NFLT_2 0x20 //Default
+#define CONFIG_NFLT_3 0x30
+#define CONFIG_NFLT_4 0x40
+#define CONFIG_NFLT_5 0x50
+#define CONFIG_NFLT_6 0x60
+#define CONFIG_NFLT_7 0x70
+
+//Spike Rejection. Robustez frente a perturbaciones
+#define CONFIG_SREJ_0 0x00
+#define CONFIG_SREJ_1 0x01
+#define CONFIG_SREJ_2 0x02 //Default
+#define CONFIG_SREJ_3 0x03
+#define CONFIG_SREJ_4 0x04
+#define CONFIG_SREJ_5 0x05
+#define CONFIG_SREJ_6 0x06
+#define CONFIG_SREJ_7 0x07
+
+//Numero mínimo de rayos
+#define CONFIG_MIN_LIGHT_0 0x00
+#define CONFIG_MIN_LIGHT_1 0x01
+#define CONFIG_MIN_LIGHT_2 0x02 //Default
+#define CONFIG_MIN_LIGHT_3 0x03
+
 // <REGISTROS>
 #define CONFIG_REG_0 0x00
 #define CONFIG_REG_1 0x01
@@ -31,10 +77,12 @@
 #define CONFIG_REG_4 0x04
 #define CONFIG_REG_5 0x05
 #define CONFIG_REG_6 0x06
-#define CONFIG_REG_7 0x07
+#define CONFIG_REG_7 0x07 
 #define CONFIG_REG_8 0x08
-#define CALIB_RCO 0x3D      // Registro para calibrar automáticamente los osciladores internos
-#define PRESET_DEFAULT 0x3C      // Registro para resetear los registros
+#define CONFIG_REG_3A 0x3A
+#define CONFIG_REG_3B 0x3B
+#define REG_CALIB_RCO 0x3D      // Registro para calibrar automáticamente los osciladores internos
+#define REG_PRESET_DEFAULT 0x3C      // Registro para resetear los registros
 
 
 int spi_fd; //Descriptor de archivo spi
@@ -72,7 +120,10 @@ void spi_write_register(uint8_t reg, uint8_t value) {
         .speed_hz = 500000,
         .bits_per_word = 8,
     };
-    ioctl(spi_fd, SPI_IOC_MESSAGE(1), &transfer);
+    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &transfer) < 0) {
+        perror("Error en comunicación SPI");
+        return; // Devuelve un valor inválido
+    }
 }
 
 void systemInit(struct gpiod_chip **chip, struct gpiod_line **line) //Los asigno como punteros a punteros
@@ -110,20 +161,28 @@ void systemInit(struct gpiod_chip **chip, struct gpiod_line **line) //Los asigno
 void applicationInit()
 {
     //-----Calibración y reset-----
-    spi_write_register(PRESET_DEFAULT, DIRECT_COMMAND); // Comando de reset
+    spi_write_register(REG_PRESET_DEFAULT, DIRECT_COMMAND); // Comando de reset
     usleep(2000); // Espera 2ms (datasheet pagina 36)
-    spi_write_register(CALIB_RCO, DIRECT_COMMAND); // Comando de calibración RCO
+    spi_write_register(REG_CALIB_RCO, DIRECT_COMMAND); // Comando de calibración RCO
+
+    printf("CONFIG_REG_1: 0x%02X\n", spi_read_register(CONFIG_REG_1)); //CREO QUE LO QUE SE HACE MAL ES LA LECTURA. ANALIZADOR LÓGICO
 
     //-----Configuraciones-----
     spi_write_register(CONFIG_REG_0, AFE_GAIN_INDOOR | NORMAL_MODE); // Modificación ganancia AFE y modo de operación
     uint8_t gain = spi_read_register(CONFIG_REG_0);
     printf("Ganancia AFE: 0x%02X\n", gain);
 
-    spi_write_register(CONFIG_REG_1, 0X25); //Mirar datasheet y crear defines para el segundo parámetro de este registro!!!!!!!!!!!!!
-    spi_write_register(CONFIG_REG_2, 0XC2); //Mirar datasheet y crear defines para el segundo parámetro de este registro!!!!!!!!!!!!!
+    spi_write_register(CONFIG_REG_1, CONFIG_NFLT_2 | CONFIG_WDTH_4); //Watchdog threshold y NFL
+    spi_write_register(CONFIG_REG_2, CONFIG_MIN_LIGHT_0 | CONFIG_SREJ_1); //Spike rejection
 
-    spi_write_register(CONFIG_REG_3, spi_read_register(CONFIG_REG_3) | 0x20); //Habilida MASK_DIST para ignorar interferencias
+    //spi_write_register(CONFIG_REG_3, spi_read_register(CONFIG_REG_3) | 0x20); //Habilida MASK_DIST para ignorar interferencias
     spi_write_register(CONFIG_REG_8, 0x00);
+
+    //------Trazas de comprobaciones----------
+    printf("CONFIG_REG_1: 0x%02X\n", spi_read_register(CONFIG_REG_1));
+    printf("CONFIG_REG_2: 0x%02X\n", spi_read_register(CONFIG_REG_2));
+    printf("CONFIG_REG_3: 0x%02X\n", spi_read_register(CONFIG_REG_3));
+    printf("CONFIG_REG_8: 0x%02X\n", spi_read_register(CONFIG_REG_8));
 
     usleep(300000); // Espera 300ms (como en codigo de ejemplo)
     printf("Sistema inicializado\n");
@@ -140,7 +199,8 @@ void handle_interrupt(struct gpiod_line *line) {
 
     if (value == 1) {
         // Leer el registro 0x03 del AS3935 para saber qué tipo de evento ocurrió
-        uint8_t event = spi_read_register(0x03) & 0x0F;
+        uint8_t event = spi_read_register(CONFIG_REG_3) & 0x0F;
+        uint8_t PRUEBA = spi_read_register(CONFIG_REG_3) & 0x0F;
         do {
             usleep(1000); 
         } while (gpiod_line_get_value(line) == 1); //Compruebo que IRQ vuelve a 0 después de haber leído el registro de interrupción
@@ -168,6 +228,7 @@ void handle_interrupt(struct gpiod_line *line) {
                 break;
             default:
                 printf("Evento desconocido (0x%02X)\n", event);
+                printf("DEBERÍA DE SER 0X00 (0x%02X)\n", PRUEBA);
         }
     }
 }
